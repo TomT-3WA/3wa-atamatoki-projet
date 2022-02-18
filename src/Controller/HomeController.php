0b7ca081bc347deb526d22bee65f30c919344a89
TomT-3WA <tom.timsit@3wa.io>
2022-02-18 17:25:05 +0100

    mailer and template for mail config

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\TrackRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -44,8 +48,34 @@ class HomeController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('$0.html.twig', []);
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $email = (new Email())
+                ->from($contact['email'])
+                ->to('tom.timsit@3wa.io')
+                ->subject('Hello ! Nouveau contact')
+                ->text(
+                    $this->renderView(
+                        'emails/contact.html.twig',
+                        compact('contact')
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($email);
+
+            $this->addFlash('message', 'Un mail a bien été envoyé');
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/contact.html.twig', [
+            'contactForm' => $form->createView()
+        ]);
     }
 }
