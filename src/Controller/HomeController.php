22c86d9991863012f4b070b8ded6de1cfaf7905f
TomT-3WA <tom.timsit@3wa.io>
2022-02-18 17:00:30 +0100

    config nav route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrackRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,40 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(TrackRepository $trackRepository): Response
     {
+        $tracks = $trackRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tracks' => $tracks,
         ]);
+    }
+
+    /**
+     * @Route("/tracks", name="tracks")
+     */
+    public function tracks(TrackRepository $trackRepository): Response
+    {
+        $tracks = $trackRepository->findAll();
+
+        return $this->render('home/tracks.html.twig', [
+            'tracks' => $tracks
+        ]);
+    }
+
+    /**
+     * @Route("/ata", name="atamatoki")
+     */
+    public function ata(): Response
+    {
+        return $this->render('home/ata.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('$0.html.twig', []);
     }
 }
